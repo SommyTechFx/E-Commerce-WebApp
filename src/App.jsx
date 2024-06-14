9daea86e3f0c0ca02f159d69bbeb7e116e71c2fb
Aningwu chisom <152747011+SommyTechFx@users.noreply.github.com>
2024-06-14 18:50:32 +0100

    corrected the display problem

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="page1" index element={<Hero />} />
+          <Route path="/" index element={<Hero />} />
           <Route path="page2" element={<Hero2 />} />
           <Route path="page3" element={<Hero3 />} />
         </Routes>
